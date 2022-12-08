cb7d2b0fb62bae9702fcdf24efe5b01efb3faa38
afu <elliotsaii1@gmail.com>
2022-12-08 13:15:39 +0800

    fix port 3000

--- ucenter-api/src/main/java/com/techguy/admin/controller/AdminRegisterController.java
+++ ucenter-api/src/main/java/com/techguy/admin/controller/AdminRegisterController.java
@@ -2,7 +2,6 @@ package com.techguy.admin.controller;
 
 import com.techguy.constant.CommonConstant;
 import com.techguy.constant.ErrorConstantMsg;
-import com.techguy.constant.SysConstant;
 import com.techguy.entity.Member;
 import com.techguy.entity.admin.Admin;
 import com.techguy.response.MessageResult;
@@ -22,7 +21,7 @@ import java.util.Date;
 @RestController
 @RequestMapping("/admin/api")
 @CrossOrigin(origins = {"http://154.39.248.73:8818","http://localhost:3000"})
-//@PreAuthorize("hasRole('ROLE_ADMIN')")
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 @Slf4j
 @AllArgsConstructor
 public class AdminRegisterController {
@@ -40,8 +39,8 @@ public class AdminRegisterController {
 
         // check code
         code = code.toLowerCase();
-
-        Object checkCode = redisTemplate.opsForValue().get(SysConstant.EMAIL_BIND_CODE_PREFIX+email);
+        String realKey = MD5Util.MD5Encode(code, "utf-8");
+        Object checkCode = redisTemplate.opsForValue().get(realKey);
         //check code
         if (checkCode == null || checkCode.equals("") || !code.equals(checkCode)) {
             result.setMessage(ErrorConstantMsg.VERIFY_CODE_WRONG);
